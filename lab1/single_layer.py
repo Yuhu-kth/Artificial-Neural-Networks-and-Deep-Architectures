6d4cb105b986a0f835ac0743d39389095dd91a08
oecz17 <54900430+oecz17@users.noreply.github.com>
2020-01-21 11:25:58 +0100

    Update single_layer.py

--- lab1/single_layer.py
+++ lab1/single_layer.py
@@ -15,12 +15,13 @@ def Delta_rule(X, T, W):
 def bias(x):
 	bias = np.ones((1,x.shape[1]))
 	x = np.append(x, bias, axis=0)
-	return 
+	return x
 
 def firstW(x,t):
 	n = x.shape[0]
 	m = t.shape[0]
 	w = np.random.randn(0,(m,n))
+	return w
 
 etha = 0.001
 n = 100
@@ -36,6 +37,16 @@ classA[1][:] = np.random.randn(1,n) * sigma + mA[1]
 classB[0][:] = np.random.randn(1,n) * sigma + mB[0]
 classB[1][:] = np.random.randn(1,n) * sigma + mB[1]
 
+X = np.concatenate((classA,classB), axis=1)
+
+t = np.ones((n*2))
+t[n:] += -2
+
+X = bias(X)
+#perceptron
+W = firstW(X,t)
+
+epoch = 20
 
 plt.scatter(classA[0][:],classA[1][:])
 plt.scatter(classB[0][:], classB[1][:])
