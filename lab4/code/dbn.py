1d3a32fd2d9ae746ce133f8263e2eff73b56168f
oecz17 <54900430+oecz17@users.noreply.github.com>
2020-02-27 14:48:11 +0100

    Update dbn.py

--- lab4/code/dbn.py
+++ lab4/code/dbn.py
@@ -86,7 +86,9 @@ class DeepBeliefNet():
         print ("accuracy = %.2f%%"%(100.*np.mean(np.argmax(predicted_lbl,axis=1)==np.argmax(true_lbl,axis=1))))
         
         return
-    def generate(self,true_lbl,name):
+
+    
+        def generate(self,true_lbl,name):
         
         """Generate data from labels
 
@@ -113,19 +115,23 @@ class DeepBeliefNet():
         #prob_topVis = np.random.uniform(0,1,(lbl.shape[0], top.bias_v.shape[0]))
         #topVis = np.random.binomial(1,prob_topVis)
 
-        prob_topVis = np.random.uniform(0,1,(lbl.shape[0], top.bias_v.shape[0]))
+        prob_topVis = np.random.uniform(0,1,(1,top.bias_v.shape[0]))
+        #print(prob_topVis.shape)
         top_vis = np.random.binomial(1, prob_topVis)
 
         for _ in range(self.n_gibbs_gener):
             top_vis[:,-lbl.shape[1]:] = lbl
             _,topH = top.get_h_given_v(top_vis)
             top_vis,topVis = top.get_v_given_h(topH)
-            print(topVis.shape)
-            print(topVis[0,:-lbl.shape[1]].shape)
-            print(lbl.shape)
+            #print(topVis.shape)
+            #print(topVis[0,:-lbl.shape[1]].shape)
+            #print(lbl.shape)
 
-            _,visPen = pen.get_v_given_h(topVis[0,:-lbl.shape[1]])
-            vis,_ = hid.get_v_given_h(vis)
+            _,visPen = pen.get_v_given_h_dir(topVis[:,:-lbl.shape[1]])
+            vis,_ = hid.get_v_given_h_dir(visPen)
+
+            plt.imshow(vis.reshape(self.image_size))
+            plt.show()
             
             records.append( [ ax.imshow(vis.reshape(self.image_size), cmap="bwr", vmin=0, vmax=1, animated=True, interpolation=None) ] )
             
